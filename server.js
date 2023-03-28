a636c771439bcde279251d067f57efe385045e10
deckkilyani <deckkilyani@gmail.com>
2023-03-28 09:40:54 +0530

    create-payment-intent updated

--- server.js
+++ server.js
@@ -10,23 +10,47 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(express.static("public"));
 app.use(express.json());
 
-const calculateOrderAmount = (items) => {
+/* const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
   return 10000;
+}; */
+
+const calculateOrderAmount = (items) => {
+  let total = 0;
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    total += item.price * item.quantity;
+  }
+  return total*100;
 };
 
+
 app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
-
-
-
+  /* const { items } = req.body; */
+  const items = [
+    { 
+      name: 'Coffee',
+      price: 10,
+      product_id: 'PKDH12',
+      quantity: 2
+    },
+    { 
+      name: 'Tea',
+      price: 8.25,
+      product_id: 'PKDH03',
+      quantity: 3
+    }
+  ];
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "krw",
+    currency: "usd",
     customer: "cus_NZhP2re3RpGNJe",
+    metadata: {
+      items: JSON.stringify(items),
+    },
     automatic_payment_methods: {
       enabled: true,
     },
